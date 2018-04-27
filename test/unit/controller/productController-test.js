bffc83651e2d7d59effddd4988c39170aa26abb5
Carlos Marinho <carlos.junior@oi.net.br>
2018-04-27 12:10:49 -0300

    adicionado o sandbox do teste unitario product

--- test/unit/controller/productController-test.js
+++ test/unit/controller/productController-test.js
@@ -3,9 +3,19 @@ const sinon = require('sinon');
 productController = require('../../../controller/Product');
 
 describe('Teste do controller product', () => {
+    let sandbox = null;
+
+    beforeEach(()=>{
+        sandbox = sinon.sandbox.create();
+    })
+
+    afterEach(()=>{
+        sandbox.restore();
+    })
+
     describe('Product:: Metodo View', () => {
         it('Deve retornar um json com todos os produtos', (done) => {
-            
+
         })
     })
 })
