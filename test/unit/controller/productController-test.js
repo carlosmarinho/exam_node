aad9746f8c39212bf65dfb292e4c64f017beae61
Carlos Marinho <carlos.junior@oi.net.br>
2018-04-27 12:09:03 -0300

    Iniciado os testes unitário do produto

---
+++ test/unit/controller/productController-test.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect
+const sinon = require('sinon');
+productController = require('../../../controller/Product');
+
+describe('Teste do controller product', () => {
+    describe('Product:: Metodo View', () => {
+        it('Deve retornar um json com todos os produtos', (done) => {
+            
+        })
+    })
+})
