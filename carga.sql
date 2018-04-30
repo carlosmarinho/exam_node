aa8179b09bf0a7233a9d491859e04a8320e85e67
Carlos <carluizfla@hotmail.com>
2018-04-30 15:08:44 -0300

    atualização do banco de dados q foi esquecido

--- carga.sql
+++ carga.sql
@@ -1,13 +1,11 @@
-CREATE TABLE `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `first_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
- `last_name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
- `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
- `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
- `created` datetime NOT NULL,
- `modified` datetime NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `exam` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `exam`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `product`
+--
 
 CREATE TABLE `product` (
   `id` int(11) NOT NULL,
@@ -18,11 +16,65 @@ CREATE TABLE `product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Fazendo dump de dados para tabela `product`
+-- Extraindo dados da tabela `product`
 --
 
 INSERT INTO `product` (`id`, `name`, `date`, `price`, `photo`) VALUES
 (1, 'A', '2018-04-27 09:43:58', 30.00, NULL),
 (2, 'B', '2018-04-27 09:43:58', 40.00, NULL),
 (4, 'C', '2018-04-27 17:39:08', 40.00, 'minha foto'),
-(5, 'D', '2018-04-27 17:39:08', 50.00, 'minha foto');
+(5, 'D', '2018-04-27 17:39:08', 50.00, 'minha foto'),
+(9, 'carlos mais', '2018-04-30 13:57:22', 23.00, 'dsfdsfds');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `first_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '1234abcd'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 'admin', 'carlos', 'silva', 'carluizfla@hotmail.com', '12345678');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO
